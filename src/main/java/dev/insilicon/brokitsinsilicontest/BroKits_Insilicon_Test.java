6265acce77297ccb0f08d10e38492b9874218b2c
Jack <jackbaker321@icloud.com>
2024-07-06 11:54:50 -0500

    Init Push

---
+++ src/main/java/dev/insilicon/brokitsinsilicontest/BroKits_Insilicon_Test.java
@@ -0,0 +1,17 @@
+package dev.insilicon.brokitsinsilicontest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BroKits_Insilicon_Test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
